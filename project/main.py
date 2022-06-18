fd201bba9729ee039d417bc0ad9a8dff7b616555
Rushil Desai <rushildesai01@gmail.com>
2022-06-18 20:17:47 +0530

    fixed csv error

--- project/main.py
+++ project/main.py
@@ -1,13 +1,8 @@
 import pandas as pd
-# from naive_bayes import NB
-# from BERT import BERT
-# from Albert import Albert
-# from xlnet import XLNET
-
-from models.BERT import BERT
-from models.Albert import Albert
-from models.xlnet import XLNET
-from models.naive_bayes import NB
+from naive_bayes import NB
+from BERT import BERT
+from Albert import Albert
+from xlnet import XLNET
 
 from os import path
 import glob
@@ -59,7 +54,7 @@ if __name__ == "__main__":
             {'epochs': 3, 'weight_decay': 0.01, 'learning_rate': 2e-5, 'adam_beta1': 0.8, 'adam_beta2': 0.9}
         ]
         for p in parameters:
-            # model = BERT(filename, p)
+            model = BERT(filename, p)
             a, t = model.pipeline()
             score.append(a)
             time.append(t)
@@ -69,7 +64,7 @@ if __name__ == "__main__":
             {'epochs': 3, 'weight_decay': 0.01, 'learning_rate': 2e-5, 'adam_beta1': 0.8, 'adam_beta2': 0.9}
         ]
         for p in parameters:
-            # model = Albert(filename, p)
+            model = Albert(filename, p)
             a, t = model.pipeline()
             score.append(a)
             time.append(t)
@@ -79,16 +74,16 @@ if __name__ == "__main__":
             {'epochs': 3, 'weight_decay': 0.01, 'learning_rate': 2e-5, 'adam_beta1': 0.8, 'adam_beta2': 0.9}
         ]
         for p in parameters:
-            # model = XLNET(filename, p)
+            model = XLNET(filename, p)
             a, t = model.pipeline()
             score.append(a)
             time.append(t)
 
-        score = pd.DataFrame([score], columns = ["Dataset", "NB", "Bert_1", "Albert_1", "XLNet_1"])
-        time = pd.DataFrame([time], columns = ["Dataset", "NB", "Bert_1", "Albert_1", "XLNet_1"])
-        scores = scores.append(score, ignore_index = True)
-        times = times.append(time, ignore_index = True)
-        print(score.head())
+        score_df = pd.DataFrame([score], columns = ["Dataset", "NB", "Bert_1", "Albert_1", "XLNet_1"])
+        time_df = pd.DataFrame([time], columns = ["Dataset", "NB", "Bert_1", "Albert_1", "XLNet_1"])
+        scores = scores.append(score_df, ignore_index = True)
+        times = times.append(time_df, ignore_index = True)
+        print(scores.head())
 
-    score.to_csv('accuracy.csv', index=False)
+    scores.to_csv('accuracy.csv', index=False)
     times.to_csv('training_time.csv', index=False)
